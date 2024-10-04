0e6c234b70f0128c8d66c9cc023de403230d3b1d
Pardhasaradhi <mekalapardhu1@gmail.com>
2024-10-04 12:03:49 +0530

    ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,20 @@
-import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 import Footer from "./components/Footer";
-
-const Home = lazy(() => import("./components/Home"));
-const TechEvents = lazy(() => import("./pages/TechEvents"));
-const NonTechEvents = lazy(() => import("./pages/NonTechEvents"));
+import TechEvents from "./pages/TechEvents";
+import NonTechEvents from "./pages/NonTechEvents";
 
 function App() {
   return (
     <>
-      <div>
-        <Suspense fallback={<div className="text-red-600">Loading...</div>}>
-          <div>
-            <BrowserRouter>
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/tech-events" element={<TechEvents />} />
-                <Route path="/non-tech-events" element={<NonTechEvents />} />
-              </Routes>
-              <Footer />
-            </BrowserRouter>
-          </div>
-        </Suspense>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/tech-events" element={<TechEvents />} />
+          <Route path="/non-tech-events" element={<NonTechEvents />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
